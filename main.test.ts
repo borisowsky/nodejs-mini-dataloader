79c387eeef6fcdf13621612184101582ed7812bd
Nikita Borisowsky <nikitowsky@gmail.com>
2021-05-08 22:51:25 +0200

    Add test

---
+++ main.test.ts
@@ -0,0 +1,21 @@
+import { MiniDataLaoder } from './main';
+import type { DataLoaderKey } from './main';
+
+const asyncLoaderImplementation = (keys: DataLoaderKey[]) => {
+  return new Promise((resolve) => {
+    queueMicrotask(() => resolve(keys.map((key) => key ** 2)));
+  });
+};
+
+describe('MiniDataLaoder', () => {
+  it('Should be executed `n` times due to `n` ticks', async () => {
+    const asyncLoader = jest.fn().mockImplementation(asyncLoaderImplementation);
+    const loader = new MiniDataLaoder(asyncLoader);
+
+    await Promise.all([loader.load(1), loader.load(2), loader.load(3)]);
+    await Promise.all([loader.load(1), loader.load(2), loader.load(3)]);
+    await Promise.all([loader.load(1), loader.load(2), loader.load(3)]);
+
+    expect(asyncLoader).toHaveBeenCalledTimes(3);
+  });
+});
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-type DataLoaderKey = number;
+export type DataLoaderKey = number;
 type DataLoaderResolver = (value: unknown) => void;
 
-class MiniDataLaoder {
+export class MiniDataLaoder {
   private batchLoaderFn: (keys: DataLoaderKey[]) => Promise<any>;
   private queue: Set<{
     key: DataLoaderKey;
@@ -17,11 +17,6 @@ class MiniDataLaoder {
     this.queue = new Set();
 
     if (keysArray.length > 0) {
-      console.log(
-        'Executed once with keys:',
-        keysArray.map((item) => item.key),
-      );
-
       this.batchLoaderFn(keysArray.map((k) => k.key)).then((values) => {
         keysArray.forEach(({ resolve }, i: number) => {
           resolve(values[i]);
@@ -40,33 +35,3 @@ class MiniDataLaoder {
     return promisedValue;
   }
 }
-
-// Mock
-const loadSomeData = (keys: DataLoaderKey[]) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(keys.map((key) => key ** 2));
-    }, 1000);
-  });
-};
-
-const myCustomLoader = new MiniDataLaoder(loadSomeData);
-
-myCustomLoader.load(1).then(console.log);
-myCustomLoader.load(2).then(console.log);
-myCustomLoader.load(3).then(console.log);
-myCustomLoader.load(4).then(console.log);
-
-setTimeout(() => {
-  myCustomLoader.load(5).then(console.log);
-  myCustomLoader.load(6).then(console.log);
-  myCustomLoader.load(7).then(console.log);
-  myCustomLoader.load(8).then(console.log);
-}, 100);
-
-setTimeout(() => {
-  myCustomLoader.load(9).then(console.log);
-  myCustomLoader.load(10).then(console.log);
-  myCustomLoader.load(11).then(console.log);
-  myCustomLoader.load(12).then(console.log);
-}, 200);
